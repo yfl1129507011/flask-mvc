760a215558de354ae09874c344d1263caeedbb34
yangfeilong <1129507011@qq.com>
2017-08-01 22:40:09 +0800

    add custom error page

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,15 @@ app.config.from_object('app.common.config')
 # 引入mysql实例
 db = SQLAlchemy(app)
 
+# 自定义404错误页面
+@app.errorhandler(404)
+def pageNotFound(e):
+    return '404错误'
+
+# 自定义500错误页面
+@app.errorhandler(500)
+def internalServerError(e):
+    return '500错误'
+
 # 将controllers下的模块全部导入
 __all__ = ['controllers']
